cab6dd9c75a989512c854d55b50d4d6b55913d7e
Jeremy Sells <mrjeremysells@gmail.com>
2019-03-08 19:08:35 +0000

    Updating dotenv as it fixes the bugs with the quoted .env file

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.Config;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class WelcomeController {
 
-  private Config config;
+  private Dotenv dotenv;
 
   @Autowired
-  public WelcomeController(Config config) {
-    this.config = config;
+  public WelcomeController(Dotenv dotenv) {
+    this.dotenv = dotenv;
   }
 
   @ResponseBody
@@ -22,11 +22,11 @@ public class WelcomeController {
     return String.join(
         "\n",
         "Hello!",
-        "Welcome to " + this.config.getWithDefault("APPLICATION_NAME", ""),
-        "Description: " + this.config.getWithDefault("APPLICATION_DESCRIPTION", ""),
-        "By: " + this.config.getWithDefault("APPLICATION_VENDOR_NAME", ""),
-        "Version: " + this.config.getWithDefault("APPLICATION_VERSION_STRING", "0.0.0"),
-        "Build: " + this.config.getWithDefault("APPLICATION_BUILD_DATE", "N.A.")
+        "Welcome to " + this.dotenv.get("APPLICATION_NAME", ""),
+        "Description: " + this.dotenv.get("APPLICATION_DESCRIPTION", ""),
+        "By: " + this.dotenv.get("APPLICATION_VENDOR_NAME", ""),
+        "Version: " + this.dotenv.get("APPLICATION_VERSION_STRING", "0.0.0"),
+        "Build: " + this.dotenv.get("APPLICATION_BUILD_DATE", "N.A.")
     );
   }
 }
